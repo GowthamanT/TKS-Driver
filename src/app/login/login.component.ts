bd4bedf0b0a68148cca4a7b1587e12c0bb3ebc5e
Gowthaman T <happygowthaman@gmail.com>
2021-02-04 15:09:26 +0530

    Validation 1 Added for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  status1 = false;
+  status2 = false;
+  Username = '';
+  Password = '';
+  msg1 = '';
+  msg2 = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if(this.Username === '') {
+      this.status1 = true;
+      this.msg1 = "Username Error";
+    }
+    if(this.Password === '') {
+      this.status2 = true;
+      this.msg2 = "Password Error";
+    }
+  }
+
+  onChange1(event) {
+    this.status1 = false;
+  }
+
+  onChange2(event) {
+    this.status2 = false;
   }
 
 }
